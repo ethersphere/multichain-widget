6af94e9ecfc75865f22f09278033330af81e889f
Cafe137 <xcafe137@gmail.com>
2026-02-05 09:48:42 +0100

    feat: add batch creation steps

---
+++ src/steps/ApproveBzzStep.ts
@@ -0,0 +1,25 @@
+import { MultichainLibrary } from '@upcoming/multichain-library'
+import { FixedPointNumber } from 'cafe-utility'
+
+interface Options {
+    library: MultichainLibrary
+    temporaryAddress: `0x${string}`
+    temporaryPrivateKey: `0x${string}`
+}
+
+export function createApproveBzzStep(options: Options) {
+    return {
+        name: 'approve-bzz',
+        precondition: async () => true,
+        action: async (_stepName: string, context: Map<string, unknown>) => {
+            const nonce = await options.library.getGnosisTransactionCount(options.temporaryAddress)
+            context.set('nonce', nonce)
+            await options.library.approveGnosisBzz({
+                amount: FixedPointNumber.fromDecimalString('1000', 16).toString(),
+                spender: options.library.constants.postageStampGnosisAddress,
+                originPrivateKey: options.temporaryPrivateKey,
+                nonce
+            })
+        }
+    }
+}
---
+++ src/steps/CreateBatchStep.ts
@@ -0,0 +1,31 @@
+import { MultichainLibrary } from '@upcoming/multichain-library'
+import { Strings, Types } from 'cafe-utility'
+
+interface Options {
+    library: MultichainLibrary
+    temporaryPrivateKey: `0x${string}`
+    targetAddress: `0x${string}`
+    batchAmount: string | bigint
+    batchDepth: number
+}
+
+export function createCreateBatchStep(options: Options) {
+    return {
+        name: 'create-batch',
+        precondition: async () => true,
+        action: async (_stepName: string, context: Map<string, unknown>) => {
+            const nonce = Types.asNumber(context.get('nonce'))
+            const batchId = await options.library.createBatchGnosis({
+                amount: options.batchAmount,
+                depth: options.batchDepth,
+                originPrivateKey: options.temporaryPrivateKey,
+                immutable: false,
+                batchNonce: `0x${Strings.randomHex(64)}` as `0x${string}`,
+                bucketDepth: 16,
+                owner: Types.asHexString(options.targetAddress),
+                nonce: nonce + 1
+            })
+            context.set('batchId', batchId)
+        }
+    }
+}
