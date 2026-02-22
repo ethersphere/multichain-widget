1dd70affe46ba0d828666f9daa8cf8d20590183f
Cafe137 <xcafe137@gmail.com>
2026-02-22 13:06:19 +0100

    feat: add fallback http transports

--- src/Config.ts
+++ src/Config.ts
@@ -1,5 +1,6 @@
 import { getDefaultConfig } from '@rainbow-me/rainbowkit'
 import { convertViemChainToRelayChain } from '@relayprotocol/relay-sdk'
+import { fallback, http } from 'viem'
 import { arbitrum, base, gnosis, mainnet, optimism, polygon } from 'viem/chains'
 import { Config } from 'wagmi'
 
@@ -7,7 +8,15 @@ export const config: Config = getDefaultConfig({
     appName: 'Multichain Library',
     projectId: '5119e426ef93d637395e119c5169ad79',
     chains: [mainnet, polygon, optimism, arbitrum, base, gnosis],
-    ssr: false
+    ssr: false,
+    transports: {
+        [mainnet.id]: fallback([http('https://ethereum-rpc.publicnode.com'), http()]),
+        [polygon.id]: fallback([http('https://polygon.drpc.org'), http()]),
+        [optimism.id]: fallback([http()]),
+        [arbitrum.id]: fallback([http()]),
+        [base.id]: fallback([http()]),
+        [gnosis.id]: fallback([http('https://xdai.fairdatasociety.org'), http()])
+    }
 })
 
 export const configuredRelayChains = [mainnet, polygon, optimism, arbitrum, base, gnosis].map(
